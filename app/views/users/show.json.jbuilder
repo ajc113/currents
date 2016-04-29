cc4166d19610ecbc5d6c4bdc5891b5a02d2dd97a
AJ Costa <costa.aj@gmail.com>
2016-04-29 11:40:27 -0400

    added User scaffold

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :email, :home_port, :boat_name, :created_at, :updated_at
---
+++ db/migrate/20160429153949_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.text :home_port
+      t.string :boat_name
+
+      t.timestamps null: false
+    end
+  end
+end
