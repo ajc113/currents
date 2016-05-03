650f975ec26f5ebef7ec6f88f0b35a67803d6963
AJ Costa <costa.aj@gmail.com>
2016-05-03 17:48:15 -0400

    add json for coordinates

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,12 @@
 class Location < ActiveRecord::Base
+	SPOT_A = [
+            {lat:  42.083668, lng: -70.634360}, 
+            {lat: 42.083668, lng: -70.462698},
+            {lat:  41.926516, lng:  -70.359015}, 
+            {lat: 41.881544, lng:  -70.522497},
+          ].to_json
+
+
+
+
 end
