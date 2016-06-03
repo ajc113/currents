7c87034658eea9e740b23082921fb53fcafc7c88
AJ Costa <costa.aj@gmail.com>
2016-06-03 11:51:34 -0400

    fixes to user model to accept new attributes prior to running devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+protected
+
+ def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+  	user_params.permit(:email, :first_name, :last_name, :home_port, :state, :vessel_name)	
+ end
 end
+
+def sign_up_params
+  devise_parameter_sanitizer.sanitize(:sign_up)
+end
+end
+
+def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super # Use the default one
+    end
+  end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root 'currents#index'
+  root to: 'currents#index'
 
   resources :locations
   resources :buzzs
