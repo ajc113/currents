2dfab47fec67d9f89988ed39468dcb4869cb95c8
AJ Costa <costa.aj@gmail.com>
2016-06-06 23:24:52 -0400

    Reports update added user id and port to report view

--- app/models/location.rb
+++ app/models/location.rb
@@ -27,22 +27,11 @@ class Location < ActiveRecord::Base
    a.push ({lat: self.lat3.to_f, lng: self.lng3.to_f}) unless self.lat3.blank? || self.lng3.blank?
    a.push ({lat: self.lat4.to_f, lng: self.lng4.to_f}) unless self.lat4.blank? || self.lng4.blank?
      self.coordinates = a
-    # self.coordinates = [{lat: self.lat1.to_f, lng: self.lng1.to_f},{lat: self.lat2.to_f, lng: self.lng2.to_f},{lat: self.lat3.to_f, lng: self.lng3.to_f},{lat: self.lat4.to_f, lng: self.lng4.to_f}]
   end
 
-  # after_instantiate :yolo
-  # def yolo
-  # self.coordinates.each_with_index do |coordinate, index|
-#   end
-# end
 
-
-
-
-
- 
-          def raw(stringish)
-            stringish.to_s.html_safe
-          end
+  def raw(stringish)
+    stringish.to_s.html_safe
+  end
 
 end
