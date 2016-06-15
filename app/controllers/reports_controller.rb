49bbd393a576bd3f57a34572e25ae439960ee0da
AJ Costa <costa.aj@gmail.com>
2016-06-14 20:13:52 -0400

    current_user functionality to report, view only user reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,8 +5,7 @@ class ReportsController < ApplicationController
   # GET /reports.json
   def index
     if current_user
-       @reports = Report.all
-    # @reports = Report.where("user_id IN (?)", current_user)
+    @reports = Report.where(user_id: [current_user.id])
    else
      redirect_to new_user_session_path, notice: 'You are not logged in.'
    end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,9 @@ class Report < ActiveRecord::Base
 belongs_to :location
 belongs_to :user
 
-
+   def self.for_user user
+  
+   end
 
 # geocoded_by :general_location
 # after_validation :geocode
