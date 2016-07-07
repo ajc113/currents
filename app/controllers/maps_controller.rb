bf18b77577b3765b400ce1fb87f1173cf4412431
AJ Costa <costa.aj@gmail.com>
2016-07-06 21:44:01 -0400

    cleaned up maps view to show reports in table view

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -10,7 +10,7 @@ class MapsController < ApplicationController
 end
 
 
-
+private
     def report_params
       params.require(:report).permit(:date, :target_species, :general_location, :catch_keepers, :catch_total, :trip_summary, :primary_method, :tide, :weather, :wind, :spot, :picture, :best_bait, :trip_description, :location_id)
     end
