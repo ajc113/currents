6945351a68056abe963f751dc448a9e3e33b60e1
AJ Costa <costa.aj@gmail.com>
2016-08-11 18:30:57 -0400

    added Mandrill api and setup in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings + {
+    :address  => "smtp.mandrillapp.com",
+    :port     => 587, 
+    :user_name  => "costa.aj@gmail.com",
+    :password => ENV['MANDRILL_API_KEY'], 
+
+  }
+
+config.action_mailer.default_url_options = { :host => 'currents.herokuapp.com'}
+
 end
