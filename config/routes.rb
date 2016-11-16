c174004ed53693d9be238354e551fa3715778ca8
AJ Costa <costa.aj@gmail.com>
2016-11-16 16:09:57 -0500

    linked home page containers to feature pages

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
 
    get 'species_filter', to: 'maps#filter_by_species', as: :species_filter
 
+   get 'reports_path', to: 'reports#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
