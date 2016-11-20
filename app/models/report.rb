a54fb1f5bbec95dde80170dc8bc75545220e4658
AJ Costa <costa.aj@gmail.com>
2016-11-20 14:56:21 -0500

    added table freeze functionality for table header

--- app/models/report.rb
+++ app/models/report.rb
@@ -27,36 +27,7 @@ belongs_to :user
     # return location_json
     f
   end
-# by_star_field :date
 
-# filterrific(
-# 	  default_filter_params: { sorted_by: 'created_at_desc' },
-# 	  available_filters: [
-# 	    :sorted_by,
-# 	    :search_query,
-# 	    :with_target_species,
-# 	    :select_options,
-# 	  ]
-# 	)
-
-
-
-#    	scope :with_target_species, lambda { |target_species| where(:target_species => target_species) }  
-
-
-
-#  def self.options_for_sorted_by
-#     [
-#       ['target_species (a-z)', 'target_species_asc'],
-#       ['date (newest first)', 'created_at_desc'],
-#       ['location_id', 'location_id_asc']
-#     ]
-#  end
-# def self.options_for_select
-# end
-
-# def self.for_user user
-# end
 
 # # geocoded_by :general_location
 # # after_validation :geocode
