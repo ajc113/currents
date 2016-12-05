c6344b6b035c1a3b8f6a7f16d67ea23cc5555d15
AJ Costa <costa.aj@gmail.com>
2016-12-05 11:40:58 -0500

    fixed map info box functionality.  Changed polygon styling

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,125 +12,82 @@ def index
 end
 
 def filter
-  @target_species = params[:target_species] unless params[:target_species].blank?
-  @location = Location.find(params[:location]) unless params[:location].blank?
-  @tide = params[:tide] unless params[:tide].blank?
-  @month = params[:date].to_date.month unless params[:date].blank?
-
-  puts "@month is #{@month}".green
-
-  puts "@target_species is #{@target_species}\n"
-  puts "@location is #{@location.inspect}\n"
-  puts "@tide is #{@tide}\n"
-  puts "@date is #{@date}\n"
-
-  @reports = current_user.reports
-  @reports = @reports.selected_species(@target_species) if @target_species
-  @reports = @reports.selected_location(@location) if @location
-  @reports = @reports.selected_tide(@tide) if @tide
-  puts "@reports before date filter is #{@reports.inspect}\n".blue
-  @reports = @reports.selected_date(@month) if @month
-  # @filtered_by_date_reports = @reports.where("cast(strftime('%m', date) as int) = ?", @month)
-  # puts "@filtered_by_date_reports after date filter is #{@filtered_by_date_reports.inspect}".green
-
-  # @reports = @reports.where("created_at between (?) and (?)", start_date, end_date)
-
-  # if @target_species && @location
-  #   @reports = current_user.reports.where(target_species: @target_species, location: @location)
-  # elsif @target_species
-  #   @reports = current_user.reports.where(target_species: @target_species)
-  # elsif @location
-  #   @reports = current_user.reports.where(location: @location)
-  # else
-  #   @reports = current_user.reports 
-  # end
-
-  # @locations = @reports.collect(&:location).select{|location| location.short_name == @short_name} #you might need to add .flatten just before .select
   
-  # @reports = @reports.select{|report| @locations.include?(report.location)}
+    @target_species = params[:target_species] unless params[:target_species].blank?
+    @location = Location.find(params[:location]) unless params[:location].blank?
+    @lreports = []
+    Location.all.each do |location|
+     if  params[:target_species] != "Any"
+     #The following line used 'select' which converted reports to Array
+      # .where can not be appliec on arrays so the code was throwing errors when not selected Any
 
-  puts "@reports are #{@reports.inspect}"
-  render 'index'
+      # reports =  location.reports.select{|l| l.target_species === params[:target_species]}
+
+      #The following line has been changed to fix the error. Please confirm the output
+      reports = location.reports.where(target_species: params[:target_species])
+
+      avgrep = reports.where('date >= ?', 1.week.ago)
+      movavg = movingavg(avgrep)
+      puts "---movingavg", movavg
+     @lreports.push(location:location,reports: userreport(reports),cfile: one_locations_json(location),movingavg: movavg[:movingavg], color: movavg[:color]) 
+    else
+      avgrep = location.reports.where('date >= ?', 1.week.ago)
+      movavg = movingavg(avgrep)
+      puts "---movingavg", movavg
+      @lreports.push(location:location,reports: userreport(location.reports),cfile: one_locations_json(location),movingavg: movavg[:movingavg], color: movavg[:color])
+    end
+    end    
+    render json: @lreports
+  end
+  def movingavg(avgrep)
+    avggroup = avgrep.group('date').average('catch_keepers')
+      avarray = avggroup.map{|a,b| b.to_f}
+      movingavg = avarray.inject{ |sum, el| sum + el }.to_f / avarray.size
+      puts "----moving average",  movingavg 
+       n = avarray.size 
+      sum_sqr = avarray.map {|x| x * x}.reduce(&:+)
+      sum_sqr = 0 if sum_sqr.to_f.nan? || sum_sqr.nil?
+      movingavg = 0 if movingavg.to_f.nan?
+      std_dev = Math.sqrt((sum_sqr - n * movingavg * movingavg)/(n-1))
+      if std_dev > 1 
+        color = "#FF3E38"
+      elsif std_dev > 0
+        color = "#C1AF6A"
+      else
+        color = "#4562A8"
+      end
+      @movingavg = {movingavg: movingavg,color: color }
+      @movingavg
+  end
+  def userreport(reports)
+    @rep = []
+    i = 0
+    reports.each do |rep|
+      @rep.push({rep: rep, vessel_name: rep.user.try(:vessel_name)})
+    end
+    @rep
+  end
+  def one_locations_json(location)
+    f = File.read location.coordinate_file.path
+
+    # puts "f is #{f.inspect}".green
+    # # f = f[0]
+    # puts "f[0] is #{f}".blue
+    # location_json = JSON.parse(f)
+    # puts "location_json is #{location_json}".green
+    # return location_json
+    puts "f ", eval(f).to_a
+    eval(f).to_a
+  end
+
+
 end
 
-    # @reports = Report.all
-    # @hash = Gmaps4rails.build_markers(@reports) do |report, marker|
-    # marker.lat report.latitude
-    # marker.lng report.longitude
-    # marker.infowindow report.trip_summary
-    # @locations = Location.where(location_params)
 
-    # end
-
-  # GET /reports/1
-  # GET /reports/1.json
-  def show
-    @reports = Report.all
-  end
-
-  # GET /reports/new
-  def new
-    @report = Report.new
-   
-  end
-
-  # GET /reports/1/edit
-  def edit
-  end
-
-  # POST /reports
-  # POST /reports.json
-  def create
-    @report = Report.new(report_params)
-    @report.user=current_user
-
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to reports_url, notice: 'Report was successfully created.' }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
-  def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reports/1
-  # DELETE /reports/1.json
-  def destroy
-    @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+private
     def report_params
       params.require(:report).permit(:date, :target_species, :general_location, :catch_keepers, :catch_total, :trip_summary, :primary_method, :tide, :weather, :wind, :spot, :picture, :best_bait, :trip_description, :location_id)
     end
     def location_params
       @location = Location.where(params[:short_name])
     end
-end
-
-
