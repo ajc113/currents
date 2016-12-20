23359be26fc2ece7308fb3bad9ffad4914a6d409
AJ Costa <costa.aj@gmail.com>
2016-12-20 17:52:04 -0500

    fixed devise user/edit form to update database, fixed styling to bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :home_port, :state, :vessel_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :home_port, :state, :vessel_name])
   end
 
   helper_method :current_user
 
   private
-
+  
   
 end
 
