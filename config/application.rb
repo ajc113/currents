38bb6efe77bc639ca451807b66eadb6a996744a6
AJ Costa <costa.aj@gmail.com>
2017-01-04 13:17:17 -0500

    clean commit, prior to deleting public/assets contents to fix heruko precompile issue

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Currents
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    config.assets.enabled = true
+
   end
 end
