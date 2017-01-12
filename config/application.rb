4c06e0535a7faf10cbb724a7e4d0295d340dbf0d
AJ Costa <costa.aj@gmail.com>
2017-01-12 17:32:23 -0500

    change default production.rb settings to   config.serve_static_files = true

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 require 'carrierwave/orm/activerecord'
 module Currents
   class Application < Rails::Application
-    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
