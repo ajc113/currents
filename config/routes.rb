e0cc3c510dd5bfadbd69be246d53f4c9b2cbd9ea
AJ Costa <costa.aj@gmail.com>
2017-01-13 10:16:03 -0500

    adding test javascript for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 
   get 'reports_path', to: 'reports#index'
  
-
+  get 'test', to: 'currents#test'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
