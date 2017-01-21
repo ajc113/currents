ed3d99fb4be7eba9635fe120dc365f0d1942c4a2
Santosh Subbarao <santosh@kavasa.in>
2017-01-21 11:54:05 +0530

    Added Logs

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -87,7 +87,9 @@ def filter_by_species
     @rep = []
     i = 0
     reports.each do |rep|
-      puts "@rep user", rep.user
+      puts "@rep user", rep.user.inspect
+      puts "@rep user", rep.user..try(:vessel_name)
+      puts "@rep user3"
       @rep.push({rep: rep, vessel_name: rep.user.try(:vessel_name)})
     end
     @rep
