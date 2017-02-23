4fae572702e6f7588ce44b13ebb03559018be43b
Kartikey Tanna <tannakartikey@gmail.com>
2017-02-23 18:32:13 +0530

    buzz model test completed

---
+++ spec/models/buzz_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+RSpec.describe Buzz do
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:post) }
+    it { is_expected.to validate_length_of(:post).is_at_most(180).on(:create) }
+  end
+
+  it "does not allow length to be more than 180" do
+  end
+end
