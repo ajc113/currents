605a7b6a364f6649b1a243f8a235c247ee3cef9f
Kartikey Tanna <tannakartikey@gmail.com>
2017-03-15 00:30:04 +0530

    added specie_id column to reports table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308215829) do
+ActiveRecord::Schema.define(version: 20170314185300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20170308215829) do
     t.integer  "location_id"
     t.integer  "user_id"
     t.date     "date"
+    t.integer  "specie_id"
   end
 
   add_index "reports", ["location_id"], name: "index_reports_on_location_id", using: :btree
