e6776faacbfecacce1e1b97e12298a6be94a651b
AJ Costa <costa.aj@gmail.com>
2017-03-13 22:49:10 -0400

    test fix to location admin method error

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -5,32 +5,48 @@ ActiveAdmin.register Location do
 #
 permit_params :short_name, :long_name, :state_waters, :demographic, :coordinates, :lat1, :lng1, :lat2, :lng2, :lat3, :lng3, :lat4, :lng4, :coordinate_file
 
- form do |f|
-    f.inputs do
-      f.input :short_name
-      f.input :long_name
-      f.input :state_waters
-      f.input :demographic
-      if action_name == 'new'
-        # f.input :lat1
-        # f.input :lng1
-        # f.input :lat2
-        # f.input :lng2
-        # f.input :lat3
-        # f.input :lng3
-        # f.input :lat4
-        # f.input :lng4
-        # f.input :coordinates, as: :text
-        f.input :coordinate_file
-      elsif action_name == 'edit'
-        # f.input :coordinates, as: :text
-        f.input :coordinate_file
-      end
-    end
-    f.actions
-  end
+
+index do
 
 
+
+  column :short_name
+  column :long_name
+  column :state_waters
+  column :demographic
+
+end
+
+
+
+
+
+ # form do |f|
+ #    f.inputs do
+ #      f.input :short_name
+ #      f.input :long_name
+ #      # f.input :state_waters
+ #      # f.input :demographic
+ #      if action_name == 'new'
+ #        # f.input :lat1
+ #        # f.input :lng1
+ #        # f.input :lat2
+ #        # f.input :lng2
+ #        # f.input :lat3
+ #        # f.input :lng3
+ #        # f.input :lat4
+ #        # f.input :lng4
+ #        # f.input :coordinates, as: :text
+ #        f.input :coordinate_file
+ #      elsif action_name == 'edit'
+ #        # f.input :coordinates, as: :text
+ #        f.input :coordinate_file
+ #      end
+ #    end
+ #    f.actions
+ #  end
+
+end
 
 #
 # or
@@ -42,4 +58,4 @@ permit_params :short_name, :long_name, :state_waters, :demographic, :coordinates
 # end
 
 
-end
+
