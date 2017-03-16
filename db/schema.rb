b1d952230c086910d3efeb317a054a1b4b3e9511
Kartikey Tanna <tannakartikey@gmail.com>
2017-03-16 18:35:58 +0530

    #55 added number column to locations table Every location name has given number which is also present in the reference image So that, while loggin catch report, users can cross match any location from map with select input

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314185300) do
+ActiveRecord::Schema.define(version: 20170316130416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20170314185300) do
     t.string   "coordinate_file"
     t.text     "state_waters"
     t.text     "demographic"
+    t.integer  "number"
   end
 
   create_table "reports", force: :cascade do |t|
