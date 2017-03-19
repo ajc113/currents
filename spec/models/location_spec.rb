f840098e47ec09f4ba30dd398dfc6e0a71392df4
Kartikey Tanna <tannakartikey@gmail.com>
2017-03-19 12:01:03 +0530

    fixed failing tests

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe Location do
+
+  describe "Coordinate file should be present" do
+  	# it { is_expected.to validate_presence_of(:coordinate_file) }
+  end
+
+  describe "Checking the sum total" do
+    it "if the color is blue(#4562A8)" do
+
+    end
+
+    it "if the color is yellow(E8C85A)" do
+    end
+
+    it "if the color is red(FF3E38)" do
+    end
+  end
+end
