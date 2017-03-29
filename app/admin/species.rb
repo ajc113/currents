6d49b0d57f95361a47aad9f8a3a6877e630ebfef
Kartikey Tanna <tannakartikey@gmail.com>
2017-03-29 20:07:21 +0530

    under sidebar added associated reports' link to species' view page in admin side

--- app/admin/species.rb
+++ app/admin/species.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register Species do
 
 permit_params :name, :state_waters, :located
+    
+    sidebar "Reports", only: [:show, :edit] do
+        ul do
+            li link_to "Reports", admin_species_reports_path(resource)
+        end
+    end
 
   index do
     selectable_column
