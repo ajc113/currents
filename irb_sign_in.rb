c3618b25973fb167c921d12ad17ccc3b2a11993a
Kartikey Tanna <tannakartikey@gmail.com>
2017-04-13 17:26:57 +0530

    created a helper for location rendering

--- irb_sign_in.rb
+++ irb_sign_in.rb
@@ -3,4 +3,4 @@ csrf_token = app.session[:_csrf_token]
 
 app.post('/users/sign_in', { "authenticity_token" => csrf_token, "user" => { "email" => "tannakartikey@gmail.com", "password" => "password" } })
 
-app.controller.current_user
+@current_user = app.controller.current_user
