e2195ff0549d2eae20b9351c2c47dc11425e3903
Kartikey Tanna <tannakartikey@gmail.com>
2017-04-17 16:44:58 +0530

    removed commented test for file uploading

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,18 +1,14 @@
 RSpec.describe Location do
 
-  describe "Coordinate file should be present" do
-  	# it { is_expected.to validate_presence_of(:coordinate_file) }
-  end
+	describe "Checking the sum total" do
+		it "if the color is blue(#4562A8)" do
+		end
 
-  describe "Checking the sum total" do
-    it "if the color is blue(#4562A8)" do
+		it "if the color is yellow(E8C85A)" do
+		end
 
-    end
+		it "if the color is red(FF3E38)" do
+		end
+	end
 
-    it "if the color is yellow(E8C85A)" do
-    end
-
-    it "if the color is red(FF3E38)" do
-    end
-  end
 end
