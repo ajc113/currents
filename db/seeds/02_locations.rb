515e870092c1282fa45297e49bda9791e15ecd7f
AJ Costa <costa.aj@gmail.com>
2017-04-21 15:10:52 -0400

    renumbered locations by state

--- db/seeds/02_locations.rb
+++ db/seeds/02_locations.rb
@@ -1,335 +1,279 @@
 location_list = [
   {
     number: 1,
-    short_name: "Stellwagen Bank - (NWC) Northwest Corner",
-    long_name: "Northwest Corner of Stellwagen Bank south to middlebank covering, the western edge to middlebank.",
-    state_waters: "MA",
-    demographic: "Offshore"
-  },
-  {
-    number: 2,
-    short_name: "Stellwagen Bank - Middlebank",
-    long_name: "Middlebank south to the notch",
-    state_waters: "MA",
-    demographic: "Offshore"
-  },
-  {
-    number: 3,
-    short_name: "Stellwagen Bank - (SWC) South West Corner",
-    long_name: "The notch south to the sw corner",
-    state_waters: "MA",
-    demographic: "Offshore"
-  },
-  {
-    number: 4,
-    short_name: "Stellwagen Bank - (SEC) South East Corner",
-    long_name: "Middle bank east to the edge",
-    state_waters: "MA",
-    demographic: "Offshore"
-  },
-  {
-    number: 5,
-    short_name: "Stellwagen Bank - Eastside",
-    long_name: "Waters east of middlebank out to WildCat Knoll",
-    state_waters: "MA",
-    demographic: "Offshore"
-  },
-  {
-    number: 6,
-    short_name: "Cape Cod Backside",
-    long_name: "Golfball, backside beaches down to nauset inlet",
+    short_name: "Cape Cod - Race Point",
+    long_name: "Race Point, Bathhouse to Wood End",
     state_waters: "MA",
     demographic: "Inshore"
   },
-  {
-    number: 7,
+    {
+    number: 2,
     short_name: "Cape Cod - Peaked Hill Bar",
     long_name: "Peaked Hill Bar down to golfball",
     state_waters: "MA",
     demographic: "Inshore"
   },
   {
-    number: 8,
-    short_name: "Cape Cod - Race Point",
-    long_name: "Race Point, Bathhouse to Wood End",
+    number: 3,
+    short_name: "Cape Cod Backside",
+    long_name: "Golfball, backside beaches down to nauset inlet",
     state_waters: "MA",
     demographic: "Inshore"
   },
   {
-    number: 9,
+    number: 4,
     short_name: "Cape Cod - Provincetown",
     long_name: "Provincetown Harbor to Pamet River",
     state_waters: "MA",
     demographic: "Inshore"    
   },
   {
-    number: 10,
+    number: 5,
     short_name: "Cape Cod - Billingsgate Shoals",
     long_name: "Billingsgate shoals, The path south to Brewster flats",
     state_waters: "MA",
     demographic: "Inshore"    
   },
   {
-    number: 11,
+    number: 6,
     short_name: "Cape Cod - Wellfleet Harbor",
     long_name: "Wellfleet Harbor to Brewster Flats",
     state_waters: "MA",
     demographic: "Inshore"
   },
   {
-    number: 12,
+    number: 7,
     short_name: "Cape Cod Bay - South",
     long_name: "Canal, Scorton Creek to Barnstable, including the Fingers",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 13,
+    number: 8,
     short_name: "Cape Cod Bay - (SW )Southwest",
     long_name: "PowerPlant, Manomet and White Cliffs down to canal",
     state_waters: "MA",
     demographic: "Inshore"
   },
   {
-    number: 14,
+    number: 9,
     short_name: "Cape Cod Bay - (NW ) Northwest",
     long_name: "Green Harbor south to the PowerPlant.  Included Farnham, High Pines Ledge and the Gurnet.",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 15,
+    number: 10,
     short_name: "Cape Cod Canal",
     long_name: "The Ditch",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 16,
+    number: 11,
     short_name: "South Shore - Plymouth Bay",
     long_name: "Plymouth, Duxbury, and Kingston Bays",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 17,
+    number: 12,
     short_name: "South Shore - Scituate",
     long_name: "BrantRock, North River, Scituate, to Minots including offshore ledges",
     state_waters: "MA",
     demographic: "Inshore"
   },
   { 
-    number: 18,
+    number: 13,
     short_name: "Boston - Inner Harbor",
     long_name: "Boston Harbor out to Dear Island, Pt Allerton",
     state_waters: "MA",
     demographic: "Inshore"
   },
   {
-    number: 19,
+    number: 14,
     short_name: "Boston - Outer Harbor",
     long_name: "Boston Outer Harbor to  Nahant Bay, offshore ledges down to the glades.",
     state_waters: "MA",
     demographic: "Inshore"
   },
   {
-    number: 20,
+    number: 15,
     short_name: "North Shore",
     long_name: "Marblehead, Salem Sound, Gloucester to Thatcher Island",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 21,
+    number: 16,
     short_name: "North Shore - Merrimack",
     long_name: "Halibut Point, Plum island north to the Merrimack.",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 22,
+    number: 17,
     short_name: "Cape Cod - Whiting Grounds",
     long_name: "Whiting Grounds to to the gulley",
     state_waters: "MA",
     demographic: "Inshore"    
   },
   {
-    number: 23,
-    short_name: "Jeffreys Ledge - West",
-    long_name: "West portion of Jeffreys Ledge",
-    state_waters: "MA",
-    demographic: "Offshore"    
-  },
-  {
-    number: 24,
-    short_name: "Jeffreys Ledge - East",
-    long_name: "Eastern portions of Jeffreys Ledge",
-    state_waters: "MA",
-    demographic: "Offshore"     
-  },
-  {
-    number: 25,
+    number: 18,
     short_name: "Cape Cod - Chatham",
     long_name: "Nauset Inlet to Chatham Harbor out to the 3 mile line",
     state_waters: "MA",
     demographic: "Inshore"     
   },
   {
-    number: 26,
+    number: 19,
     short_name: "Cape Cod - Monomoy",
     long_name: "Monomoy Shoals",
     state_waters: "MA",
     demographic: "Inshore"  
   },
   {
-    number: 27,
+    number: 20,
     short_name: "Cape Cod - Nantucket Sound East",
     long_name: "Eastern Nantucket sound out to Monomoy.  Hyanis south to Muskeget Island",
     state_waters: "MA",
     demographic: "Inshore"      
   },
   {
-    number: 28,
+    number: 21,
     short_name: "Cape Cod - Nantucket Sound West",
     long_name: "Western Nantucket sound from Falmouth to Hyannis south to Cape Poge.  Includes hedge fence, and horseshoe shoals.",
     state_waters: "MA",
     demographic: "Inshore"   
   },
   {
-    number: 29,
+    number: 22,
     short_name: "Vineyard Sound",
     long_name: "Vineyard Sound  and Elizabeth islands including Quick's, Robinson's, and Wood's hole.  Cuttyhunk to Falmouth.",
     state_waters: "MA",
     demographic: "Inshore"   
   },
   {
-    number: 30,
-    short_name: "New Hampshire Coast",
-    long_name: "Hampton beach to Portsmouth",
-    state_waters: "MA",
-    demographic: "Inshore"   
-  },
-  {
-    number: 31,
+    number: 23,
     short_name: "Buzzards Bay - West",
     long_name: "Westport east to New Bedford.  Includes Dartmouth, Cuttyhunk and Gosnold, Naushon Island.",
     state_waters: "MA",
     demographic: "Inshore"   
   },
   {
-    number: 32,
+    number: 24,
     short_name: "Buzzards Bay - East",
     long_name: "Mass Maritime west to Mattapoisett.  Includes Onset, Wareham , Marion, Pocasset, and North Falmouth.",
     state_waters: "MA",
     demographic: "Inshore"     
   },
   {
-    number: 33,
+    number: 1,
     short_name: "New Hampshire Coast",
     long_name: "Coastal New Hampshire - Hampton Beach to Portsmouth including the Isle of Shoals",
     state_waters: "NH",
     demographic: "Inshore"      
   },
     {
-    number: 34,
+    number: 1,
     short_name: "Southwest Maine",
     long_name: "South maine coast from Kittery north to Cape Elizabeth. Include York, Ogunquit, Wells, Saco.",
     state_waters: "ME",
     demographic: "Inshore"      
   },
       {
-    number: 35,
+    number: 2,
     short_name: "Casco Bay",
     long_name: "All of Casco bay to Popham",
     state_waters: "ME",
     demographic: "Inshore"      
   },
   {
-    number: 36,
+    number: 3,
     short_name: "Boothbay",
     long_name: "Popham to Bristol including Sheepscot River, Damariscotta River, Boothbay, and Bristol",
     state_waters: "ME",
     demographic: "Inshore"      
   },
   {
-    number: 37,
+    number: 4,
     short_name: "St George",
     long_name: "Briston to Camden including Friendship, St George, Thomaston to Owl's Head",
     state_waters: "ME",
     demographic: "Inshore"      
   },
   {
-    number: 38,
+    number: 5,
     short_name: "Penobscot Bay",
     long_name: "Penobscot Bay including Rockland, Camden, Penobscot River, Vinalhaven Island, to Isle Au Hout",
     state_waters: "ME",
     demographic: "Inshore"      
   },
     {
-    number: 39,
+    number: 6,
     short_name: "Mt Desert Island",
     long_name: "Jericho Bay, Blue Hill Bay, Swan Island, Mt Desert Island, Frenchman Bay",
     state_waters: "ME",
     demographic: "Inshore"      
   },
     {
-    number: 40,
+    number: 7,
     short_name: "Downeast Winter Harbor to Jonesport",
     long_name: "Downeast Maine - Schoodic Point, Great Island to Head Harbor Island",
     state_waters: "ME",
     demographic: "Inshore"      
   },
   {
-    number: 41,
+    number: 8,
     short_name: "Downeast Jonesport to Lubec",
     long_name: "Downeast Maine from Harbor Island, Machias Bay, Grand Manan Channel to Lubec",
     state_waters: "ME",
     demographic: "Inshore"      
   },
     {
-    number: 43,
+    number: 1,
     short_name: "Sakonnet River",
     long_name: "Warrent point, Sakonnett River, Mt Hope Bay, and Taunton River in MA",
     state_waters: "RI",
     demographic: "Inshore"      
   },
   {
-    number: 44,
+    number: 2,
     short_name: "Newport",
     long_name: "Galilee, Narragansett, to Newport",
     state_waters: "RI",
     demographic: "Inshore"      
   },
   {
-    number: 45,
+    number: 3,
     short_name: "Watch Hill to Pt Judith",
     long_name: "Watch Hill reef, Misquamicut, Salt Ponds, Breechway, Nebraska Shoal to Pt Judith",
     state_waters: "RI",
     demographic: "Inshore"      
   },
   {
-    number: 46,
+    number: 4,
     short_name: "Narragansett Bay Western Passage",
     long_name: "Greenwich Bay, West Jamestown, south to Beavertail point",
     state_waters: "RI",
     demographic: "Inshore"      
   },
   {
-    number: 47,
+    number: 5,
     short_name: "Narragansett Bay Eastern Passage",
     long_name: "Providence River, East Prudence Island, West Newport to Brenton Point",
     state_waters: "RI",
     demographic: "Inshore"      
   },
   {
-    number: 48,
+    number: 6,
     short_name: "Block Island West",
     long_name: "West side of the Block, Sandy Point, North Reef south to Black Rock Point.  Out 3 nautical miles including Southwest Ledge and Cable Area.",
     state_waters: "RI",
     demographic: "Inshore"      
   },
   {
-    number: 49,
+    number: 7,
     short_name: "Block Island East",
     long_name: "East side of the Block, Sandy Point out 3 nautical miles including Old Harbor to  Southeast Light.",
     state_waters: "RI",
@@ -530,7 +474,56 @@ location_list = [
     long_name: "Sound north of Montauk Pt including Cerberus Shoal",
     state_waters: "NY",
     demographic: "Inshore"      
-  },                                                  
+  },  
+  {
+    number: 1,
+    short_name: "Stellwagen Bank - (NWC) Northwest Corner",
+    long_name: "Northwest Corner of Stellwagen Bank south to middlebank covering, the western edge to middlebank.",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"
+  },
+  {
+    number: 2,
+    short_name: "Stellwagen Bank - Middlebank",
+    long_name: "Middlebank south to the notch",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"
+  },
+  {
+    number: 3,
+    short_name: "Stellwagen Bank - (SWC) South West Corner",
+    long_name: "The notch south to the sw corner",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"
+  },
+  {
+    number: 4,
+    short_name: "Stellwagen Bank - (SEC) South East Corner",
+    long_name: "Middle bank east to the edge",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"
+  },
+  {
+    number: 5,
+    short_name: "Stellwagen Bank - Eastside",
+    long_name: "Waters east of middlebank out to WildCat Knoll",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"
+  },
+  {
+    number: 6,
+    short_name: "Jeffreys Ledge - West",
+    long_name: "West portion of Jeffreys Ledge",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"    
+  },
+  {
+    number: 7,
+    short_name: "Jeffreys Ledge - East",
+    long_name: "Eastern portions of Jeffreys Ledge",
+    state_waters: "Offshore - Gulf of Maine",
+    demographic: "Offshore"     
+  },  
 ]
 
 def coordinate_file_path location_name
