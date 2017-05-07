6f012c02f2abd55107fa695a478d353ad2f7a3fb
Kartikey Tanna <tannakartikey@gmail.com>
2017-05-07 20:11:48 +0530

    added specs for the pricing page

---
+++ spec/requests/maps_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "/reports_of_location" do
+  before do
+    @new_user = create(:user)
+    login_as(@new_user, scope: :user)
+    @location = create(:location)
+    @report = create(:report, :location_id => 1)
+  end
+
+  it "returns particular fiedls of reports for a location" do
+    get "/reports_of_location?location_id=1"
+    
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq('application/json')
+  end
+
+end 
