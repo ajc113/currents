6e81b6bd64786ed0a4c41d49e5232a13e3cc6378
Kartikey Tanna <tannakartikey@gmail.com>
2017-05-14 18:46:19 +0530

    renamed state to state_waters in users changed add/edit user forms accordingly added foreign key from states to user for state_waters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :home_port, :state, :vessel_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :home_port, :state, :vessel_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :home_port, :state_waters, :vessel_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :home_port, :state_waters, :vessel_name])
   end
 
   helper_method :current_user
