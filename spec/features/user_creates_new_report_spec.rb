dd01e4a9f3dba5107d510846c602137e230603ea
Kartikey Tanna <tannakartikey@gmail.com>
2017-05-14 20:43:10 +0530

    fixed failing feature test for reports

--- spec/features/user_creates_new_report_spec.rb
+++ spec/features/user_creates_new_report_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 RSpec.feature "User opens Log Catch Report page", :js => true do
 	before do
+    State.create(name: "MA")
 		Species.create(name:"Bluefish")
     new_user = create(:user)
 		Location.create(number: 1,
@@ -14,7 +15,7 @@ RSpec.feature "User opens Log Catch Report page", :js => true do
 		visit new_report_path
 		select DateTime.now.day-1, :from => "report[date(3i)]"
 		select "Bluefish", :from => "report[species_id]"
-		select "MA", :from => "report[state]" 
+		select "MA", :from => "report[state_waters]" 
 		wait_for_ajax
 		select "01 - Stellwagen Bank - (NWC) Northwest Corner", :from => "report[location_id]"
 		fill_in "report[catch_keepers]", :with => 23
