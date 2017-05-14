4c06e44e8ea508edb2a0a2b10b897a06e62fbbf4
Kartikey Tanna <tannakartikey@gmail.com>
2017-05-14 18:50:24 +0530

    added tests for states model

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe State, type: :model do
-  it { should have_many(:locations).with_primary_key(:name).with_foreign_key(:state) }
-  it { should have_many(:reports).with_primary_key(:name).with_foreign_key(:state) }
-  it { should have_many(:users).with_primary_key(:name).with_foreign_key(:state) }
+  it { should have_many(:locations).with_primary_key(:name).with_foreign_key(:state_waters) }
+  it { should have_many(:reports).with_primary_key(:name).with_foreign_key(:state_waters) }
+  it { should have_many(:users).with_primary_key(:name).with_foreign_key(:state_waters) }
 end
