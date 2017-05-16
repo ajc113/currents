89a6e036fae58c988c6afb9e6d09ace62b237fd0
Kartikey Tanna <tannakartikey@gmail.com>
2017-05-16 20:17:12 +0530

    validates presence of state_waters for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
 	:recoverable, :rememberable, :trackable, :validatable, :confirmable
-
+  validates :state_waters, presence: true
 	has_many :reports
 	has_many :buzzs   
 	has_many :locations, through: :reports
