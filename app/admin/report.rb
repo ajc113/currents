39a1b8cd77328a24abed5b94e13773be96fd3f9c
Kartikey Tanna <tannakartikey@gmail.com>
2017-06-01 22:52:10 +0530

    changed edit report form for admin side

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -9,14 +9,8 @@ ActiveAdmin.register Report do
     actions
     column :user_id
     column :date
-
-    column "Location" do |location_id|
-      Location.find(location_id).short_name
-    end
-
-    column "Species" do |species_id|
-      Species.find(species_id).name
-    end
+    column :location
+    column :species
     column :catch_keepers
     column :catch_total
     column :trip_summary
@@ -26,15 +20,26 @@ ActiveAdmin.register Report do
     column :spot
     column :best_bait
     column :trip_description
-    column :created_at
-    column :updated_at
-    column :latitude
-    column :longitude
   end
 
-
-
-
+  form do |f|
+    inputs do
+      input :date
+      input :species
+      input :state, as: :select, include_blank: false
+      input :location
+      input :catch_keepers
+      input :trip_summary
+      input :catch_total
+      input :primary_method
+      input :tide
+      input :weather
+      input :wind
+      input :spot
+      input :best_bait
+      input :trip_description
+    end
+  end
 end
 
 
