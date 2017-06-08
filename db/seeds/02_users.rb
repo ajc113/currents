4efeb83e9c643d92dfc2eee7942731eaa2428947
Kartikey Tanna <tannakartikey@gmail.com>
2017-06-08 10:17:07 +0530

    seeding only required users. One admin user and two normal users

--- db/seeds/02_users.rb
+++ db/seeds/02_users.rb
@@ -1,19 +1,3 @@
-puts "Creating users"
-2.times do |i|
-  begin
-    User.create!(
-      email: FFaker::Internet.email,
-      confirmed_at: DateTime.now,
-      password: 'password',
-      state_waters: State.where(visible: 1).order("RANDOM()").first.name,
-    )
-    print '.'
-  rescue => error
-    puts error.backtrace
-    print 'F'
-  end
-end
-
 puts "\nCreating real users"
 
 real_users = [
@@ -33,7 +17,7 @@ real_users.each do |user|
       email: user[:email],
       password: 'password',
       confirmed_at: DateTime.now,
-      state_waters: State.where(visible: 1).order("RANDOM()").first.name,
+      state_waters: State.visible.order("RANDOM()").first.name,
     )
     print '.'
   rescue
