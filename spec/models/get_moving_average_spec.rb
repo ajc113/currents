c85b693759157b76251fbc5351ff2ed5d1e32163
Kartikey Tanna <tannakartikey@gmail.com>
2017-06-22 14:48:21 +0530

    added tests for the remaining methods of GetMovingAverage

--- spec/models/get_moving_average_spec.rb
+++ spec/models/get_moving_average_spec.rb
@@ -2,12 +2,14 @@ RSpec.describe GetMovingAverage do
   before do
     # Following is the hash of each day and catch_keepers
     # The new report will be created with each catch_keepers value
+    # In excel sheet Day 4 has a record with catch_keeper value zero.
+    # It has been removed from the test to demonstrate that if the records not
+    # found than program considers it as zero
     report = {
      8 => [1,2,0,1],
      7 => [1,2,0,1],
      6 => [1,1,2,2],
      5 => [0],
-     4 => [0],
      3 => [1,2,3,1],
      2 => [3,2,1,1,2],
      1 => [3,3,4,2,4],
@@ -15,16 +17,37 @@ RSpec.describe GetMovingAverage do
     }
     report.each do |day, catch_keepers_array|
       catch_keepers_array.each do |catch_keepers|
-        create(:report, catch_keepers: catch_keepers, date: Date.today - day)
+        FactoryGirl.create(:report, catch_keepers: catch_keepers, date: Date.today - day)
       end
     end
+    @reports = Report.all
+    @maps_data = GetMovingAverage.new(@reports) 
+  end
+
+  it "should return reports of one week including today" do
+    first_report_date = @maps_data.one_weeks_reports.first.date
+    last_report_date = @maps_data.one_weeks_reports.last.date
+    expect(first_report_date).to eq(Date.today)
+    expect(last_report_date).to eq(Date.today-6)
+  end
+
+  it "should return reports of last eight days including today" do
+    first_report_date = @maps_data.eight_days_reports.first.date
+    last_report_date = @maps_data.eight_days_reports.last.date
+    expect(first_report_date).to eq(Date.today-1)
+    expect(last_report_date).to eq(Date.today-7)
   end
 
   it "should calculate moving average for the given reports" do
-    class_instance = GetMovingAverage.new(Report.all)
-    class_instance.moving_average
-    expect(class_instance.moving_average).to eq(1.66)
-    expect(class_instance.pre_moving_average).to eq(1.32)
-    expect(class_instance.standard_deviation).to eq(0.34)
+    expect(@maps_data.moving_average).to eq(1.66)
+    expect(@maps_data.pre_moving_average).to eq(1.32)
+    expect(@maps_data.standard_deviation).to eq(0.34)
+  end
+
+  it "should return zero if no reports are passed" do
+    reports = Report.none 
+    maps_data = GetMovingAverage.new(reports) 
+    expect(maps_data.moving_average).to eq(0)
+    expect(maps_data.standard_deviation).to eq(0)
   end
 end
