3c5f0cea3391b5071647a43a40f26d0ba3e50cd5
Kartikey Tanna <tannakartikey@gmail.com>
2017-06-22 14:47:15 +0530

    refactored the code and handled null values

--- app/models/get_moving_average.rb
+++ app/models/get_moving_average.rb
@@ -4,35 +4,31 @@ class GetMovingAverage
   end
 
   def moving_average
-    @movingavg = one_week_average(one_weeks_reports, Date.today-6).round(2)
+    one_week_average(one_weeks_reports).round(2)
   end
 
   def pre_moving_average
-    @prevmovingavg = one_week_average(eight_days_reports, Date.today-7).round(2)
+    one_week_average(eight_days_reports).round(2)
   end
 
   def one_weeks_reports
-    @one_week_reports = @reports.past_week unless blank?
+    @reports.between_times(Date.today-6, Date.today) unless @reports.blank?
   end
 
   def eight_days_reports
-    @eight_days_reports = @reports.between_times(Date.today-8, Date.today-1) unless blank?
+    @reports.between_times(Date.today-7, Date.today-1) unless @reports.blank?
   end
 
   def standard_deviation
     (moving_average - pre_moving_average).round(2)
   end
 
-  def one_week_average(avgrep,startdate)
-    enddate = startdate + 6
-		avggroup = avgrep.group('date').average('catch_keepers')
-		avarray = (startdate..enddate).map {|date|
-			if avggroup[date]
-				avggroup[date].to_f
-			else
-				0.to_f
-			end
-		}
-		movingavg = avarray.reduce(:+).to_f / avarray.size
+  def one_week_average(avgrep)
+    group_average = avgrep.group('date').average('catch_keepers').map(&:last).flatten.reduce(:+) / 7 unless avgrep.blank?
+    if group_average.blank?
+      0
+    else
+      group_average
+    end
 	end
 end
