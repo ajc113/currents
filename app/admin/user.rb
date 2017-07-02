602e8006a303984193361f66938f9bf5143204c1
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-02 23:34:25 +0530

    beautified the file

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,26 +1,11 @@
 ActiveAdmin.register User do
 
-remove_filter :state
-permit_params :email, :first_name, :last_name, :home_port, :state_waters, :vessel_name, :subscription_tier
+	filter :state, as: :check_boxes
+	permit_params :email, :first_name, :last_name, :home_port, :state_waters, :vessel_name, :subscription_tier
 
-    sidebar "Reports", only: [:show, :edit] do
-        ul do
-            li link_to "Reports", admin_user_reports_path(resource)
-        end
-    end
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
-
+	sidebar "Reports", only: [:show, :edit] do
+		ul do
+			li link_to "Reports", admin_user_reports_path(resource)
+		end
+	end
 end
