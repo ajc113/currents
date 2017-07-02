52a0bce610833d68dfd5b184255f6abfd444fdf3
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-03 00:17:28 +0530

    beautified the file

--- app/admin/species.rb
+++ app/admin/species.rb
@@ -1,19 +1,18 @@
 ActiveAdmin.register Species do
 
-permit_params :name, :located
-    
-    sidebar "Reports", only: [:show, :edit] do
-        ul do
-            li link_to "Reports", admin_species_reports_path(resource)
-        end
-    end
+	permit_params :name, :located
+		sidebar "Reports", only: [:show, :edit] do
+		ul do
+			li link_to "Reports", admin_species_reports_path(resource)
+		end
+	end
 
-  index do
-    selectable_column
-    actions
-    column :name
-    column :located
-  end
+	index do
+		selectable_column
+		actions
+		column :name
+		column :located
+	end
 
 
 end
