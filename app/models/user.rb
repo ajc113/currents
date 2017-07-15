e414b8d31a4927194de6181393c02165a459f285
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-15 18:12:48 +0530

    after user is created, the customer will be created on Stripe with the same mali id the customer will be on the trial period of 31 days the custome will be subscribed to 'monthly' plan

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,69 +8,24 @@ class User < ActiveRecord::Base
 	has_many :buzzs   
 	has_many :locations, through: :reports
   belongs_to :state, primary_key: :name, foreign_key: :state_waters
+  after_create :generate_stripe_customer_id
 
   #display_name is defined for activeadmin
 	def display_name
 		self.email
 	end
 
-	#STATES = [
-		## "AL",
-		## "AK",
-		## "AZ",
-		## "AR",
-		## "CA",
-		## "CO",
-		#"CT",
-		## "DE",
-		## "DC",
-		## "FL",
-		## "GA",
-		## "HI",
-		## "ID",
-		## "IL",
-		## "IN",
-		## "IA",
-		## "KS",
-		## "KY",
-		## "LA",
-		#"ME",
-		## "MD",
-		#"MA",
-		## "MI",
-		## "MN",
-		## "MS",
-		## "MO",
-		## "MT",
-		## "NE",
-		## "NV",
-		#"NH",
-		## "NJ",
-		## "NM",
-		#"NY",
-		## "NC",
-		## "ND",
-		## "OH",
-		## "OK",
-		## "OR",
-		## "PA",
-		## "PR",
-		#"RI",
-		## "SC",
-		## "SD",
-		## "TN",
-		## "TX",
-		## "UT",
-		## "VT",
-		## "VA",
-		## "WA",
-		## "WV",
-		## "WI",
-		## "WY",
-		#"Offshore - Gulf of Maine",
-		#"Offshore - South of New England",
-		#"Offshore - East of Chatham and Georges Bank",
-		#"Offshore - Northeast Canyons"
-	#]
+  def generate_stripe_customer_id
+    customer = Stripe::Customer.create(
+      :email => self.email
+    )
 
+    @subscription = Stripe::Subscription.create(
+      :customer  => customer.id,
+      :plan      => 'monthly',
+      :trial_end => (Date.today + 31).to_time.to_i 
+    )
+    self.stripe_customer_id = customer.id
+    self.save!
+  end
 end
