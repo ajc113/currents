65dcce5502fcc7d7d675700111e732eb5c0d5ebc
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-19 18:24:16 +0530

    added is_active flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714144706) do
+ActiveRecord::Schema.define(version: 20170719124550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,18 +132,18 @@ ActiveRecord::Schema.define(version: 20170714144706) do
   add_index "states", ["name"], name: "index_states_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "home_port"
@@ -153,7 +153,9 @@ ActiveRecord::Schema.define(version: 20170714144706) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "subscription_tier"
-    t.string   "stripe_customer_id",     default: "", null: false
+    t.string   "stripe_customer_id",     default: "",   null: false
+    t.string   "subscription_id"
+    t.boolean  "is_active",              default: true, null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
