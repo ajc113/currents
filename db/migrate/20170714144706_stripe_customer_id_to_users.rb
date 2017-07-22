acbb70f902138ff0200204329d5c554467e65b87
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-22 23:19:47 +0530

    creating stripe customer for existing user through migration

--- db/migrate/20170714144706_stripe_customer_id_to_users.rb
+++ db/migrate/20170714144706_stripe_customer_id_to_users.rb
@@ -2,4 +2,9 @@ class StripeCustomerIdToUsers < ActiveRecord::Migration
   def change
     add_column :users, :stripe_customer_id, :string, default: "", null: false
   end
+  User.find_each do |user|
+    if user.stripe_customer_id == "" then
+      StripeCustomer.create(user)
+    end
+  end
 end
