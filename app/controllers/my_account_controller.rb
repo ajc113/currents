3c3ba2dbf9477ce22707990e33c856bf6e6b13e6
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-24 02:11:25 +0530

    handling null values for upcoming invoices

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -3,7 +3,11 @@ class MyAccountController < ApplicationController
     @customer = StripeCustomer.retrieve(current_user.stripe_customer_id)
     @subscription = StripeSubscription.retrieve(current_user.subscription_id) unless current_user.subscription_id.nil?
     @card = Stripe::Source.retrieve(@customer.default_source).card unless current_user.payment_source.nil?
-    @upcoming_invoice = Stripe::Invoice.upcoming(customer: @customer.id) || nil
+    @upcoming_invoice = begin 
+                          Stripe::Invoice.upcoming(customer: @customer.id) || nil
+                        rescue
+                          nil
+                        end
     @invoice_list = Stripe::Invoice.list
     @invoices = @invoice_list.data.select{ |invoice| invoice["customer"] == current_user.stripe_customer_id }
   end
