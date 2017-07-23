01c193ffef3a8dbd92fd196b5ee5968d719197ef
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-23 18:28:12 +0530

    Started showing invoice and other subscription related details on My Account page

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -1,6 +1,16 @@
 class MyAccountController < ApplicationController
   def show
     @customer = StripeCustomer.retrieve(current_user.stripe_customer_id)
+    @subscription = StripeSubscription.retrieve(current_user.subscription_id)
     @card = Stripe::Source.retrieve(@customer.default_source).card unless current_user.payment_source.nil?
+    @upcoming_invoice = Stripe::Invoice.upcoming(customer: @customer.id)
+    @invoice_list = Stripe::Invoice.list
+    @invoices = @invoice_list.data.select{ |invoice| invoice["customer"] == current_user.stripe_customer_id }
+  end
+
+  private 
+  
+  def trial_ended?
+    DateTime.now.to_i > @subscription.trial_end ? true : false
   end
 end
