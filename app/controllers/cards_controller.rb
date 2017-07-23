d9b6e2093f5cdcc158ad3f522966ab0fecb1d17f
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-24 02:36:50 +0530

    arranged begin rescue blocks

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,21 +7,23 @@ class CardsController < ApplicationController
   end
 
   def create
-    @customer = StripeCustomer.retrieve(current_user.stripe_customer_id)
-    @source = @customer.sources.create({:source => params[:stripeSource]})
-    @customer.default_source = @source.id
-    @customer.save
-    current_user.payment_source = @source.id
-    if current_user.subscription_id == nil
-      StripeSubscription.create(current_user)
+    begin
+      @customer = StripeCustomer.retrieve(current_user.stripe_customer_id)
+      @source = @customer.sources.create({:source => params[:stripeSource]})
+      @customer.default_source = @source.id
+      @customer.save
+      current_user.payment_source = @source.id
+      if current_user.subscription_id == nil
+        StripeSubscription.create(current_user)
+      end
+      respond_to do |format|
+        format.html { redirect_to my_account_path, notice: 'Card was added successfully added' }
+      end
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_card_path
     end
-    respond_to do |format|
-      format.html { redirect_to my_account_path, notice: 'Card was added successfully added' }
-    end
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_card_path
-  end
     current_user.is_active = true
     current_user.save!
+  end
 end
