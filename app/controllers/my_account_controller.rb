73b94dee10360f18ea351fccfd140901a8137906
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-24 01:40:46 +0530

    getting subscription details only if it is not blank

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -1,7 +1,7 @@
 class MyAccountController < ApplicationController
   def show
     @customer = StripeCustomer.retrieve(current_user.stripe_customer_id)
-    @subscription = StripeSubscription.retrieve(current_user.subscription_id)
+    @subscription = StripeSubscription.retrieve(current_user.subscription_id) unless current_user.subscription_id.nil?
     @card = Stripe::Source.retrieve(@customer.default_source).card unless current_user.payment_source.nil?
     @upcoming_invoice = Stripe::Invoice.upcoming(customer: @customer.id)
     @invoice_list = Stripe::Invoice.list
