68ae261d95708ef5b663dc29bdad7d4a0fecb988
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-24 02:31:04 +0530

    added user activation only after successful card addition

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -12,8 +12,6 @@ class CardsController < ApplicationController
     @customer.default_source = @source.id
     @customer.save
     current_user.payment_source = @source.id
-    current_user.is_active = true
-    current_user.save!
     if current_user.subscription_id == nil
       StripeSubscription.create(current_user)
     end
@@ -24,4 +22,6 @@ class CardsController < ApplicationController
     flash[:error] = e.message
     redirect_to new_card_path
   end
+    current_user.is_active = true
+    current_user.save!
 end
