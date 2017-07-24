f65f76b50c8a352278169737f754ce6a3f05c0f6
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-24 23:05:45 +0530

    added method to delete all the sources of a customer

--- app/models/stripe_customer.rb
+++ app/models/stripe_customer.rb
@@ -17,5 +17,14 @@ class StripeCustomer
   def self.payment_source(customer_id)
     self.retrieve(customer_id).default_source
   end
+
+  def self.delete_all_sources(user)
+    customer = self.retrieve(user)
+    customer.sources.each do |source|
+      source.delete
+    end
+    user.payment_source = nil
+    user.save!
+  end
   
 end
