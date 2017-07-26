d1e027eac90f32411c4e7240126846e02836459e
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-26 22:44:41 +0530

    allowing requests from all ip in dev environment so that ngrok can send request to the app from Stripe

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.web_console.whiny_requests = false
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
