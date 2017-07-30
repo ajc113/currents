f934c18366ea996fe363e8cdf9bc65884cebe576
Kartikey Tanna <tannakartikey@gmail.com>
2017-07-30 20:39:30 +0530

    generating mailer preview methods dynamically from the methods defined in mailer class

--- spec/mailers/previews/subscription_mailer_preview.rb
+++ spec/mailers/previews/subscription_mailer_preview.rb
@@ -1,6 +1,8 @@
-# Preview all emails at http://localhost:3000/rails/mailers/trial_end
+# Preview all emails at http://localhost:3000/rails/mailers/subscription_mailer
 class SubscriptionMailerPreview < ActionMailer::Preview
-  def customer_subscription_trial_will_end
-    SubscriptionMailer.customer_subscription_trial_will_end(User.first)
+  SubscriptionMailer.instance_methods(false).each do |method|
+    define_method method do |attr|
+      SubscriptionMailer.public_send(method, attr)
+    end
   end
 end
