66fc36b8a35813cedb19e2768ac53118e6b05406
Kartikey Tanna <tannakartikey@gmail.com>
2017-08-01 19:16:27 +0530

    using Mandrill templates to render emails

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,44 +1,40 @@
 class AdminMailer < ApplicationMailer
 	default from: "no_reply@thefishing.network"
-	default to: "currentsfishingnetwork@gmail.com"
+	#default to: "currentsfishingnetwork@gmail.com"
+	default to: "tannakartikey@gmail.com"
 
 	def new_user(user)
 		@user = user
 		mail(subject: "New User: #{user.email}")
 	end
-
-	def mandrill_client
-		@mandrill_client ||= Mandrill::API.new MANDRILL_API_KEY
-	
-		merge_vars = attributes.map do |key, value|
-      { name: key, content: value }
-
-	end
-
-	# def new_report(report)
-	# 	@report = report
-	# 	template_name = "new-report"
-	# 	message = {
-	# 		to: [{email: "currentsfishingnetwork@gmail.com"}],
-	# 		subject: "New Report",
-	# 		merge_vars: [
-	# 			{rcpt: "currentsfishingnetwork@gmail.com",
-	# 			 vars: [
-	# 			 	{name: "REPORTNEW", content: report.location.short_name},
-	# 			 	{name: "REPORTSUMMARY", content: report.trip_summary},
-	# 			 		] 
-	# 				}
- # 	      ]
- # 	    }
-
- # 	    mandrill_client.message.send_template template_name, message
- # 	 end    
-
-	end
-
-	def new_report(report)
+		def new_report(report)
 		@report = report
 		mail(subject: "New Report: #{report.location.short_name}")
 	end
+
+	def mandrill_client
+		@mandrill_client ||= Mandrill::API.new ENV['MANDRILL_API_KEY']
+		merge_vars = attributes.map do |key, value|
+			{ name: key, content: value }
+		end
+	end
+
+		# def new_report(report)
+		# 	@report = report
+		# 	template_name = "new-report"
+		# 	message = {
+		# 		to: [{email: "currentsfishingnetwork@gmail.com"}],
+		# 		subject: "New Report",
+		# 		merge_vars: [
+		# 			{rcpt: "currentsfishingnetwork@gmail.com",
+		# 			 vars: [
+		# 			 	{name: "REPORTNEW", content: report.location.short_name},
+		# 			 	{name: "REPORTSUMMARY", content: report.trip_summary},
+		# 			 		]
+		# 				}
+		# 	      ]
+		# 	    }
+		# 	    mandrill_client.message.send_template template_name, message
+		# 	 end
 
 end
--- spec/mailers/previews/admin_mailer_preview.rb
+++ spec/mailers/previews/admin_mailer_preview.rb
@@ -1,4 +1,11 @@
 # Preview all emails at http://localhost:3000/rails/mailers/admin_mailer
 class AdminMailerPreview < ActionMailer::Preview
+  def new_user
+    AdminMailer.new_user(User.first)
+  end
+
+  def new_report
+    AdminMailer.new_report(Report.first)
+  end
 
 end
