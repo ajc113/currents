18785192f4565b0ccbd6336e62c48905efe118ba
AJ Costa <costa.aj@gmail.com>
2017-08-16 18:38:18 -0400

    work on devise custom mailer, clean commit prior to PR #110

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -1,22 +1,17 @@
 class DeviseCustomMailer < Devise::Mailer
-
-	helper :application
-	default template_path: 'devise/mailer'
-	layout 'mailer'
-	# add_template_helper EmailHelper
-	add_template_helper ApplicationHelper
-	default from: 'Fishing Network <intel@thefishing.network>'
-  default to: "currentsfishingnetwork@gmail.com"
-
-	require "mandrill"
-
-  def confirmation_instructions(user, token, opts={})
-    @username = user.first_name || user.email
-    @confirmation_link = "http://" + ENV['HOST'] + "/users/confirmation?confirmation_token=" + token
-    mail(to: user.email, subject: 'Account confirmation')
-  end
-
-	
-
+  helper :application
+  default template_path: 'devise/mailer'
+  layout 'mailer'
+  # add_template_helper EmailHelper
+  add_template_helper ApplicationHelper
+  default from: 'Fishing Network <intel@thefishing.network>'
+  default to: "tannakartikey@gmail.com"
+ 
+  require "mandrill"
+ 
+  # def confirmation_instructions(user, token, opts={})
+  #   @username = user.first_name || user.email
+  #   @confirmation_link = "http://" + ENV['HOST'] + "/users/confirmation?confirmation_token=" + token
+  #   mail(to: user.email, subject: 'Account confirmation')
+  # end
 end
-
