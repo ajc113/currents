78630b1c4d44256c3de1eb8c34b9d84fd22c676e
Kartikey Tanna <tannakartikey@gmail.com>
2017-10-24 10:43:47 +0530

    Remove unnecessary lines

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,12 +24,8 @@ ActiveRecord::Migration.maintain_test_schema!
 FakeStripeRunner.boot
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
@@ -48,35 +44,17 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryGirl::Syntax::Methods
-  #Including Capybara DSL
   config.include Capybara::DSL
   Capybara.javascript_driver = :webkit
   Capybara.default_max_wait_time = 10
   Capybara::Webkit.configure do |config|
     config.allow_unknown_urls
   end
-  #Devise
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :feature
