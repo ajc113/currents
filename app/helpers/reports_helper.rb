a0ceee90f8e501ce0bb37890bca9e42e81d8805f
Kartikey Tanna <tannakartikey@gmail.com>
2017-11-02 23:42:24 +0530

    Added report helpers spec

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,23 +1,23 @@
 module ReportsHelper
-	def selected_species(species)
-		"selected" if params[:species].to_i == species
-	end
+  def selected_species(species)
+    "selected" if params[:species].to_i == species
+  end
 
   def selected_state(state)
     "selected" if params[:state] == state
   end
 
-	def selected_locations(location)
+  def selected_locations(location)
     "selected" if params[:location].to_i == location
-	end
+  end
 
-	def selected_tide(tide)
-		"selected" if params[:tide] == tide
-	end
+  def selected_tide(tide)
+    "selected" if params[:tide] == tide
+  end
 
-	def all_species
+  def all_species
     Species.where("id in (select species_id from reports)").uniq.sort
-	end
+  end
 
   def render_location_option location
     html = '<option value="' + location.id.to_s  + '">' + location.number.to_s.rjust(2, '0') + ' - ' + location.short_name + '</option>'
@@ -28,5 +28,5 @@ module ReportsHelper
     html = '<option value="' + location.id.to_s + '" ' + selected_locations(location.id).to_s + '>' + location.short_name + ' (' + location.state.name + ')' + '</option>'
     return html.html_safe
   end
-  
+
 end
