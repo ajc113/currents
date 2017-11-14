badf77796e3b79367f2e39ec3bd74a3ead5f80ca
AJ Costa <costa.aj@gmail.com>
2017-11-14 13:47:07 -0500

    clean commit prior to pr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804123545) do
+ActiveRecord::Schema.define(version: 20171108030923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,52 @@ ActiveRecord::Schema.define(version: 20170804123545) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "blogo_posts", force: :cascade do |t|
+    t.integer  "user_id",          null: false
+    t.string   "permalink",        null: false
+    t.string   "title",            null: false
+    t.boolean  "published",        null: false
+    t.datetime "published_at",     null: false
+    t.string   "markup_lang",      null: false
+    t.text     "raw_content",      null: false
+    t.text     "html_content",     null: false
+    t.text     "html_overview"
+    t.string   "tags_string"
+    t.string   "meta_description", null: false
+    t.string   "meta_image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogo_posts", ["permalink"], name: "index_blogo_posts_on_permalink", unique: true, using: :btree
+  add_index "blogo_posts", ["published_at"], name: "index_blogo_posts_on_published_at", using: :btree
+  add_index "blogo_posts", ["user_id"], name: "index_blogo_posts_on_user_id", using: :btree
+
+  create_table "blogo_taggings", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+  end
+
+  add_index "blogo_taggings", ["tag_id", "post_id"], name: "index_blogo_taggings_on_tag_id_and_post_id", unique: true, using: :btree
+
+  create_table "blogo_tags", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogo_tags", ["name"], name: "index_blogo_tags_on_name", unique: true, using: :btree
+
+  create_table "blogo_users", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogo_users", ["email"], name: "index_blogo_users_on_email", unique: true, using: :btree
+
   create_table "buzzs", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "post"
@@ -73,6 +119,74 @@ ActiveRecord::Schema.define(version: 20170804123545) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
+  create_table "lines_articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "sub_title"
+    t.text     "content"
+    t.boolean  "published",        default: false
+    t.datetime "published_at"
+    t.string   "hero_image"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "slug"
+    t.string   "gplus_url"
+    t.boolean  "featured",         default: false
+    t.string   "document"
+    t.string   "short_hero_image"
+    t.text     "teaser"
+  end
+
+  add_index "lines_articles", ["slug"], name: "index_lines_articles_on_slug", unique: true, using: :btree
+
+  create_table "lines_authorables", force: :cascade do |t|
+    t.integer  "author_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "lines_authorables", ["article_id"], name: "index_lines_authorables_on_article_id", using: :btree
+  add_index "lines_authorables", ["author_id"], name: "index_lines_authorables_on_author_id", using: :btree
+
+  create_table "lines_authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.text     "description"
+    t.string   "gplus_profile"
+  end
+
+  create_table "lines_pictures", force: :cascade do |t|
+    t.string   "image"
+    t.string   "name"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "lines_pictures", ["article_id"], name: "index_lines_pictures_on_article_id", using: :btree
+
+  create_table "lines_users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string   "short_name"
@@ -130,6 +244,25 @@ ActiveRecord::Schema.define(version: 20170804123545) do
   end
 
   add_index "states", ["name"], name: "index_states_on_name", unique: true, using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",   null: false
