cf644aa6ea3a4ed8e631ddfd027d74b86428dbc0
AJ Costa <costa.aj@gmail.com>
2017-11-17 14:26:22 -0500

    created comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116171947) do
+ActiveRecord::Schema.define(version: 20171117192434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,16 @@ ActiveRecord::Schema.define(version: 20171116171947) do
   end
 
   add_index "buzzs", ["user_id"], name: "index_buzzs_on_user_id", using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -305,6 +315,7 @@ ActiveRecord::Schema.define(version: 20171116171947) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "buzzs", "users"
+  add_foreign_key "comments", "posts"
   add_foreign_key "locations", "states", column: "state_waters", primary_key: "name"
   add_foreign_key "reports", "locations"
   add_foreign_key "reports", "states", column: "state_waters", primary_key: "name"
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
+  factory :comment do
+    name "MyString"
+    body "MyText"
+    post nil
+  end
   factory :post do
     title "MyString"
     body "MyText"
