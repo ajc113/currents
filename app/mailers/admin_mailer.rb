d55d5c502ce575ff7e548e4d76dabf2d5217eee2
Kartikey Tanna <tannakartikey@gmail.com>
2017-11-20 09:59:28 +0530

    Beautified

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,25 +1,25 @@
 class AdminMailer < ApplicationMailer
-	default from: "no_reply@thefishing.network"
-  	default to: AdminEmails.list
+  default from: "no_reply@thefishing.network"
+  default to: AdminEmails.list
 
 
-	def new_user(user)
+  def new_user(user)
     @username = user.first_name || user.email
     @confirmation_link = "http://" + ENV['HOST'] + "/users/confirmation?confirmation_token=" + user.confirmation_token
-		@user = user
-		mail(subject: "New User: #{user.email}")
-	end
-	
-	def new_report(report)
-		@report = report
-		mail(subject: "New Report: #{report.location.short_name}")
-	end
+    @user = user
+    mail(subject: "New User: #{user.email}")
+  end
 
-	def mandrill_client
-		@mandrill_client ||= Mandrill::API.new ENV['MANDRILL_API_KEY']
-		merge_vars = attributes.map do |key, value|
-			{ name: key, content: value }
-		end
-	end
+  def new_report(report)
+    @report = report
+    mail(subject: "New Report: #{report.location.short_name}")
+  end
+
+  def mandrill_client
+    @mandrill_client ||= Mandrill::API.new ENV['MANDRILL_API_KEY']
+    merge_vars = attributes.map do |key, value|
+      { name: key, content: value }
+    end
+  end
 
 end
