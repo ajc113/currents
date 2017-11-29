ac66fa720fd3a7e07bb7354cadb7c98fe75e28b3
AJ Costa <costa.aj@gmail.com>
2017-11-29 11:42:39 -0500

    added link redirect to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
 	def create
-		@post = Post.find(params[:post_id])
+		@post = Post.friendly.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment].permit(:name, :body))
 
 		redirect_to post_path(@post)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,19 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  before_save :anti_spam
+ 
+
+ def anti_spam
+    doc = Nokogiri::HTML::DocumentFragment.parse(body)
+
+    doc.css('a').each do |a|
+      a[:rel] = 'nofollow'
+      a[:target] = '_blank'
+    end
+
+    self.body = doc.to_s
+  end
+
+
+
 end
