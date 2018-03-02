afb8023f5f330d1c2a5d25442e1ba18f87a7073e
Kartikey Tanna <tannakartikey@gmail.com>
2018-03-02 16:37:58 +0530

    added name_or_email method for devise mailer
    Whenever the first_name is empty, email of the user
    will be used in gretting.

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -9,13 +9,18 @@ class DeviseCustomMailer < Devise::Mailer
   require "mandrill"
 
   def confirmation_instructions(user, token, opts={})
-    @username = user.first_name || user.email
+    @username = name_or_email user
     @confirmation_link = "http://" + ENV['HOST'] + "/users/confirmation?confirmation_token=" + token
     mandrill = Mandrill::API.new ENV['MANDRILL_API_KEY']
     template_name = "registration"
     template_content = []
     merge_vars = [{"content"=> @username, "name"=>"USER_NAME"}, {"content"=> @confirmation_link, "name"=>"ACCOUNT_CONFIRMATION_LINK"}]
     @result = (mandrill.templates.render template_name, template_content, merge_vars)['html']
-    mail(to: user.email, subject: 'Account confirmation', content_type: 'text/html')
+    mail(to: user.email, subject: 'Account confirmation')
+  end
+
+  def name_or_email user
+    return user.email if user.first_name.blank?
+    user.first_name
   end
 end
