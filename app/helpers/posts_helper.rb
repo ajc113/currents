85f3b53e86e13609ef0999164ead7ae9a9bf8b86
Kartikey Tanna <tannakartikey@gmail.com>
2019-03-15 19:42:58 +0530

    #214 Spots index page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,6 @@
 module PostsHelper
   include ActsAsTaggableOn::TagsHelper
 
-  def post_tags post
-    return '-' if post.tags.empty?
-    raw post.tags.map { |tag| tag_link(tag) }.join(", ")
-  end
-
   def post_format post
     return 'image' if post.image_url
     return 'video' if post.youtube_id.present?
