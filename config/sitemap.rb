78fd88cbd4f0acad045e9f80bc36cb9ad4a41699
AJ Costa <costa.aj@gmail.com>
2019-06-12 10:57:11 -0400

    site map update to currentsfihing

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.thefishing.network"
+SitemapGenerator::Sitemap.default_host = "https://www.currentsfishing.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
