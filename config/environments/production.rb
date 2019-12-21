8e8ff047adfcc6f5c46ca938f42121ffbba77aa5
Kartikey Tanna <tannakartikey@gmail.com>
2019-12-21 15:28:21 +0530

    Enabled compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   
-  config.assets.compile = false
+  config.assets.compile = true
  
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
