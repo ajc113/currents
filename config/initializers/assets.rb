d11556ef7365f1598278065d0e742260c409fd15
Kartikey Tanna <tannakartikey@gmail.com>
2019-12-22 08:25:51 +0530

    Added assets to precompile manually

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( maps.css )
 Rails.application.config.assets.precompile += %w( currents.css )
+Rails.application.config.assets.precompile += %w(daybreak.jpg hotspot.png tuna-halfbeaks.jpg favicon.png)
+Rails.application.config.assets.precompile += %w(application.css application.js)
 Rails.application.config.assets.precompile += %w( ckeditor/*)
